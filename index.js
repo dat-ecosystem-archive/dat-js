e75e5fe66749f0ea4c600c58c63b539bf52122b9
RangerMauve <rangermauve@hotmail.com>
2019-02-14 14:44:49 -0500

    Updated to ES6

--- index.js
+++ index.js
@@ -1,70 +1,72 @@
-var inherits = require('util').inherits
-var xtend = require('xtend')
-var events = require('events')
+const inherits = require('util').inherits
+const xtend = require('xtend')
+const EventEmitter = require('events').EventEmitter
 
-var Repo = require('./repo')
+const Repo = require('./repo')
 
-module.exports = Dat
+module.exports =
 
 /**
  * The Dat object. Manages multiple repositories in
  * a single discovery-swarm instance.
  * @param {Object} opts   Default options to use for the dat.
  */
-function Dat (opts) {
-  if (!(this instanceof Dat)) return new Dat(opts)
-  events.EventEmitter.call(this)
-  this.opts = opts || {}
-  this.repos = []
-}
-
-inherits(Dat, events.EventEmitter)
-
-/**
- * Returns a repo with the given url. Returns undefined
- * if no repository is found with that url.
- * @param  {url} url      The url of the repo.
- * @return {Repo|undefined}  The repo object with the corresponding url.
- */
-Dat.prototype.get = function (url) {
-  var repos = this.repos.filter(function (repo) {
-    return repo.url = url
-  })
-  if (repos.length) return repos[0]
-  return this.add(url)
-}
-
-/**
- * Adds a new dat. Emits a 'repo' event when the undelying archive
- * instance is open.
- * @param {string}   url   The url to the dat.
- * @param {object}   opts  Options to use when building the dat.
- */
-Dat.prototype.add = function (url, opts) {
-  var self = this
-  if (self.destroyed) throw new Error('client is destroyed')
-  if (!opts) opts = {}
-
-  var repo = new Repo(url, xtend(this.opts, opts))
-  self.repos.push(repo)
-
-  setTimeout(() => {
-    this.emit('repo', repo)
-  }, 0)
-  return repo
-}
-
-/**
- * Closes the dat, the swarm, and all underlying repo instances.
- */
-Dat.prototype.destroy =
-Dat.prototype.close = function (cb) {
-  if(cb) this.once('close', cb)
-
-  while (this.repos.length) {
-    var repo = this.repos.pop()
-    repo.close()
+class Dat extends EventEmitter {
+  constructor (opts) {
+    super()
+    this.opts = opts || {}
+    this.repos = []
   }
-  this.destroyed = true
-  this.emit('close')
+
+  /**
+   * Returns a repo with the given url. Returns undefined
+   * if no repository is found with that url.
+   * @param  {url} url      The url of the repo.
+   * @return {Repo|undefined}  The repo object with the corresponding url.
+   */
+  get (url) {
+    const repos = this.repos.filter((repo) => repo.url === url)
+    if (repos.length) return repos[0]
+    return this.add(url)
+  }
+
+  /**
+   * Adds a new dat. Emits a 'repo' event when the undelying archive
+   * instance is open.
+   * @param {string}   url   The url to the dat.
+   * @param {object}   opts  Options to use when building the dat.
+   */
+  add (url, opts) {
+    const self = this
+    if (self.destroyed) throw new Error('client is destroyed')
+    if (!opts) opts = {}
+
+    const repo = new Repo(url, xtend(this.opts, opts))
+    self.repos.push(repo)
+
+    setTimeout(() => {
+      this.emit('repo', repo)
+    }, 0)
+
+    return repo
+  }
+
+  /**
+   * Closes the dat, the swarm, and all underlying repo instances.
+   */
+  close (cb) {
+    if(cb) this.once('close', cb)
+
+    while (this.repos.length) {
+      const repo = this.repos.pop()
+      repo.close()
+    }
+
+    this.destroyed = true
+    this.emit('close')
+  }
+
+  destroy (cb) {
+    this.close(cb)
+  }
 }
--- repo.js
+++ repo.js
@@ -1,120 +1,116 @@
-var inherits = require('util').inherits
-var events = require('events')
-var WebrtcSwarm = require('@geut/discovery-swarm-webrtc')
-var Signalhub = require('signalhub')
-var hyperdrive = require('hyperdrive')
-var ram = require('random-access-memory')
-var websocket = require('websocket-stream')
+const inherits = require('util').inherits
+const EventEmitter = require('events').EventEmitter
+const WebrtcSwarm = require('@geut/discovery-swarm-webrtc')
+const Signalhub = require('signalhub')
+const hyperdrive = require('hyperdrive')
+const ram = require('random-access-memory')
+const websocket = require('websocket-stream')
 
-var DEFAULT_WEBSOCKET_RECONNECT = 1000
-var DAT_PROTOCOL = 'dat://'
+const DEFAULT_WEBSOCKET_RECONNECT = 1000
+const DAT_PROTOCOL = 'dat://'
 
-var DEFAULT_SIGNALHUBS = ['http://gateway.mauve.moe:3463']
+const DEFAULT_SIGNALHUBS = ['http://gateway.mauve.moe:3463']
 
-module.exports = Repo
+module.exports =
 
 /**
  * A dat repository is a hyperdrive with some default settings.
  * @param {string} url    The url
  * @param {Object} opts   Options to use in the archive instance
  */
-function Repo (url, opts) {
-  if (!(this instanceof Repo)) return new Repo(url, opts)
-  events.EventEmitter.call(this)
-
-  var key = null;
-  if(url) {
-    if(url.indexOf(DAT_PROTOCOL) === 0) {
-      key = url.slice(DAT_PROTOCOL.length)
-    } else {
-      key = url
-      url = DAT_PROTOCOL + key
+class Repo extends EventEmitter {
+  constructor (url, opts) {
+    super()
+    let key = null;
+    if(url) {
+      if(url.indexOf(DAT_PROTOCOL) === 0) {
+        key = url.slice(DAT_PROTOCOL.length)
+      } else {
+        key = url
+        url = DAT_PROTOCOL + key
+      }
     }
+
+    this.url = url
+    this.opts = opts || {}
+    this.db = this.opts.db || ram
+    this.archive = hyperdrive(this.db, key, opts)
+    this._isReady = false
+
+    if(!url) {
+      this.ready(() => {
+        const url = 'dat://' + this.archive.key.toString('hex')
+        this.url = url
+      })
+    }
+
+    this._open()
   }
 
-  this.url = url
-  this.opts = opts || {}
-  this.db = this.opts.db || ram
-  this.archive = hyperdrive(this.db, key, opts)
-  this._isReady = false
+  _createWebsocket (server) {
+    const url = server + '/' + this.archive.key.toString('hex')
 
-  if(!url) {
-    this.ready(() => {
-      var url = 'dat://' + this.archive.key.toString('hex')
-      this.url = url
+    this.websocket = websocket(url)
+
+    this.websocket.once('error', () => {
+      setTimeout(() => {
+        this._createWebsocket(server)
+      }, this.opts.websocketReconnectDelay || DEFAULT_WEBSOCKET_RECONNECT)
+    })
+
+    this.websocket.pipe(this.archive.replicate({
+      sparse: true,
+      live: true
+    })).pipe(this.websocket)
+  }
+
+  _createWebrtcSwarm () {
+    // TODO: Detect whether the page is HTTPS or not in order to set the protocol
+    const signalhub = Signalhub(this.archive.key.toString('hex'), this.opts.signalhub || DEFAULT_SIGNALHUBS)
+    const swarm = WebrtcSwarm({
+      hash: false,
+      stream: () => this.archive.replicate()
+    })
+
+    this.swarm = swarm
+
+    swarm.join(signalhub)
+
+    return swarm
+  }
+
+  _open () {
+    this.archive.ready(() => {
+      this._createWebrtcSwarm()
+      if (this.opts.websocketServer) this._createWebsocket(this.opts.websocketServer)
+      this._isReady = true
+      this.emit('ready')
     })
   }
 
-  this._open()
-}
-
-inherits(Repo, events.EventEmitter)
-
-Repo.prototype._createWebsocket = function (server) {
-  var url = server + '/' + this.archive.key.toString('hex')
-
-  this.websocket = websocket(url)
-
-  this.websocket.once('error', () => {
-    setTimeout(() => {
-      this._createWebsocket(server)
-    }, this.opts.websocketReconnectDelay || DEFAULT_WEBSOCKET_RECONNECT)
-  })
-
-  this.websocket.pipe(this.archive.replicate({
-    sparse: true,
-    live: true
-  })).pipe(this.websocket)
-}
-
-Repo.prototype._createWebrtcSwarm = function () {
-  // TODO: Detect whether the page is HTTPS or not in order to set the protocol
-  var signalhub = Signalhub(this.archive.key.toString('hex'), this.opts.signalhub || DEFAULT_SIGNALHUBS)
-  var swarm = WebrtcSwarm({
-    hash: false,
-    stream: () => this.archive.replicate()
-  })
-
-  this.swarm = swarm
-
-  swarm.join(signalhub)
-
-  return swarm
-}
-
-Repo.prototype._open = function () {
-  var self = this
-  this.archive.ready(function () {
-    self._createWebrtcSwarm()
-    if (self.opts.websocketServer) self._createWebsocket(self.opts.websocketServer)
-    self._isReady = true
-    self.emit('ready')
-  })
-}
-
-Repo.prototype.ready = function(cb) {
-  if(this._isReady) {
-    setTimeout(cb, 0)
-  }
-  this.once('ready', cb)
-}
-
-Repo.prototype.destroy =
-Repo.prototype.close = function (cb) {
-  var self = this
-
-  if(cb) this.once('close', cb)
-
-  for (let channel of self.swarm.channels.values()) {
-    channel.swarm.close()
+  ready (cb) {
+    if(this._isReady) {
+      setTimeout(cb, 0)
+    }
+    this.once('ready', cb)
   }
 
-  if (this.websocket) {
-    this.websocket.end()
-    this.websocket = null
+  close (cb) {
+    if(cb) this.once('close', cb)
+
+    if (this.websocket) {
+      this.websocket.end()
+      this.websocket = null
+    }
+
+    this.swarm.close(() => {
+      this.archive.close(() => {
+        this.emit('close')
+      })
+    })
   }
 
-  self.archive.close(function () {
-    self.emit('close')
-  })
+  destroy (cb) {
+    this.close(cb)
+  }
 }
